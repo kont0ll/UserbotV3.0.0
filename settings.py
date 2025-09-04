2ad6c43d502453861a613571eb089922af9210c2
kont0ll <raksssgaming@gmail.com>
2025-09-04 08:40:40 +0700

    settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # === TELEGRAM OWNER ===
-OWNER_ID = 7868229705
+OWNER_ID = 7721682757
 OWNER_NAME = "JUN NOT DEVS"
 GROUP_CHAT = "junofficial354gc"
 CHANNEL = "junofficial354ch"
@@ -9,7 +9,7 @@ IP = "IP BOKEP INI JANGAN DI GANTI 😹"
 BOT_NAME = "TIKUS KANTOR DPR BABI"
 PP = "https://files.catbox.moe/zzx0s5.jpg" # foto bokep ini bebas 🗿
 BUG_URL = "https://files.catbox.moe/zzx0s5.jpg"
-BOT_TOKEN = "BERHARAP_ADA_TOKENNYA_KAH?"
+BOT_TOKEN = "7996951701:AAE9Kqi_c4BWg03aZk2D1Q7PnG5KTUZ2Y8g"
 # === PTERODACTYL PANEL ===
 DOMAIN = "https://junaoffc.panelmurmer.web.id"
 PLTA = "ptla_ZeiAUwnqme299jhutwvbKYpOS0AWDUORRKJQtG4qbdR"
@@ -18,8 +18,8 @@ LOC = 1
 EGGS = 15
 
 # === TELEGRAM API (my.telegram.org) ===
-API_ID = 999999
-API_HASH = "XNXXBOKEPGACOR123"
+API_ID = 22885923
+API_HASH = "4d9e7f9d6f988f9ee528661ccc93f41e"
 
 
 # apa ga bisa ambil api hash ama api id? A N A K  P I N T A R 😊
